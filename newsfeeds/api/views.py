9bc0cb1b07af86159181b989edc58ed647f6d083
Qian Chen <qyc5937@gmail.com>
2021-05-10 19:43:54 -0700

    cleanup comment in newsfeed view

--- newsfeeds/api/views.py
+++ newsfeeds/api/views.py
@@ -17,36 +17,3 @@ class NewsFeedViewSet(viewsets.GenericViewSet):
     def list(self, request):
         newsfeeds = NewsFeed.objects.filter(user_id=self.request.user)
         return Response({'newsfeed': NewsfeedSerializer(newsfeeds, many=True).data}, status=HTTP_200_OK)
-'''
-    def create(self, request):
-        serialzier = TweetSerializerForCreate(
-            data=request.data,
-            context={'request': request},
-        )
-
-        if not serialzier.is_valid():
-            return Response({
-               "success": False,
-               "message": "Please check input.",
-               "errors": serialzier.errors,
-            }, status=400)
-
-        tweet = serialzier.save()
-        return Response(TweetSerializer(tweet).data, status=201)
-
-
-    def get_permissions(self):
-        if self.action == 'list':
-            return [AllowAny()]
-        return [IsAuthenticated()]
-
-    def list(self, request):
-        if 'user_id' not in request.query_params:
-            return Response('Invalid user_id', status=400)
-
-        tweets = Tweet.objects.filter(
-            user_id=request.query_params['user_id']
-        ).order_by('-created_at')
-        serializer = TweetSerializer(tweets, many=True)
-        r
-'''
