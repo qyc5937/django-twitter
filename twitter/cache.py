a70d74963a9e1599eb4112e2d9d32714e93cea01
Qian Chen <qyc5937@gmail.com>
2021-07-05 17:38:46 -0700

    add newsfeed redis cache

--- twitter/cache.py
+++ twitter/cache.py
@@ -4,4 +4,5 @@ USER_PATTERN='user:{user_id}'
 USER_PROFILE_PATTERN='userprofile:{user_id}'
 
 #redis
-USER_TWEET_PATTERN = 'user_tweet:{user_id}'
+USER_TWEET_PATTERN = 'user_tweets:{user_id}'
+USER_NEWSFEEDS_PATTERN = 'user_newsfeeds:{user_id}'
